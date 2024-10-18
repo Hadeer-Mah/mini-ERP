c4745785b8e7fedb58144c4c30632c83c8c5019d
Hadeer Mahmoud <hadeermahmoud952013@gmail.com>
2024-10-18 21:16:44 +0300

    Establishing Json server and Listing & Searching Employees

--- src/components/SharedComponents/Searchbar/Searchbar.jsx
+++ src/components/SharedComponents/Searchbar/Searchbar.jsx
@@ -1,10 +1,16 @@
 import search from "../../../assets/svgs/search.svg"
-export default function Searchbar() {
+export default function Searchbar({state, setState, placeholder}) {
   return (
     <div className="border border-[#E2E2E2] pl-14 pr-4 py-2 rounded-[50px] relative w-full">
-      <img src={search} alt="search" className="absolute left-6"/>
+      <img src={search} alt="search" className="absolute left-6 w-[24px]"/>
       <input
         type="search"
+        className="w-full text-sm"
+        placeholder={placeholder}
+        value={state}
+        onChange={(e)=>{
+          setState(e.target.value)
+        }}
       />
     </div>
   )
--- src/pages/Employees/Employees.jsx
+++ src/pages/Employees/Employees.jsx
@@ -1,75 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataTable from "../../components/SharedComponents/DataTable/DataTable";
 import MainButton from "../../components/SharedComponents/MainButton/MainButton";
 import Searchbar from "../../components/SharedComponents/Searchbar/Searchbar";
 import AddEmployee from "./AddEmployee/AddEmployee";
+import trash from "../../assets/svgs/dataTable/trash.svg";
+import active from "../../assets/svgs/dataTable/active.svg";
+import inActive from "../../assets/svgs/dataTable/inActive.svg";
+
 
 export default function Employees() {
-  const [isAddingEmployee, setIsAddingEmployee] = useState(false)
-  console.log(isAddingEmployee)
-  const employeesData = [
-    {
-      id: 1,
-      Employee: "Ahmed",
-      Role: "IT",
-      Email: "example@gmail.com",
-      Phone: "010102039",
-      "Start Date": "1/1/2020",
-      Active: true,
-      "": "delete icon",
-    },
-    {
-      id: 2,
-      Employee: "Ahmed2",
-      Role: "IT",
-      Email: "example@gmail.com",
-      Phone: "010102039",
-      "Start Date": "1/1/2020",
-      Active: true,
-      "": "delete icon",
-    },
-    {
-      id: 3,
-      Employee: "Ahmed3",
-      Role: "IT",
-      Email: "example@gmail.com",
-      Phone: "010102039",
-      "Start Date": "1/1/2020",
-      Active: true,
-      "": "delete icon",
-    },
-  ];
+  const [employeesList, setEmployeesList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isAddingEmployee, setIsAddingEmployee] = useState(false);
+
+  const formattedEmployees = employeesList?.map((employee) => {
+    return {
+      "Employee": employee.Employee,
+      "Start Date": employee.StartDate,
+      "Phone": employee.Phone,
+      "Email": employee.Email,
+      "Role": employee.Role,
+      "image": employee.image,
+      "Active": <img src={employee.Active ? active : inActive} alt="status"className="w-[20px]"/>,
+      "": <img src={trash} alt="delete" className="cursor-pointer w-[20px]"/>,
+    };
+  });
+
+  useEffect(() => {
+    const url = searchTerm
+      ? `http://localhost:8000/employees?Employee=${encodeURIComponent(searchTerm)}`
+      : "http://localhost:8000/employees";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setEmployeesList(data))
+      .catch((error) => console.error("Error fetching employees:", error));
+  }, [searchTerm]);
+
   function openModalFunction() {
     setIsAddingEmployee(true);
   }
   return (
     <>
-    {isAddingEmployee && (
-        <AddEmployee openModal={isAddingEmployee} setOpenModal={setIsAddingEmployee} />
-      )}
-    <div className="rounded-2xl p-3 bg-white w-full">
-      <div className="flex items-center gap-2">
-        <div className="w-[calc(100%-170px)]">
-          <Searchbar />
-        </div>
-        <MainButton btnTitle={"+ New Employee"} onClickFn={openModalFunction}/>
-      </div>
-      <div className="my-6 overflow-hidde w-full">
-        <DataTable
-          mapKey={"id"}
-          tableColumns={[
-            "Employee",
-            "Role",
-            "Email",
-            "Phone",
-            "Start Date",
-            "Active",
-            "",
-          ]}
-          tableRows={employeesData}
+      {isAddingEmployee && (
+        <AddEmployee
+          openModal={isAddingEmployee}
+          setOpenModal={setIsAddingEmployee}
         />
+      )}
+      <div className="rounded-2xl p-3 bg-white w-full">
+        <div className="flex items-center gap-2">
+          <div className="w-[calc(100%-170px)]">
+            <Searchbar state={searchTerm} setState={setSearchTerm} placeholder={"Search Employees by Name"}/>
+          </div>
+          <MainButton
+            btnTitle={"+ New Employee"}
+            onClickFn={openModalFunction}
+          />
+        </div>
+        <div className="my-6 overflow-hidde w-full">
+          <DataTable
+            mapKey={"id"}
+            tableColumns={[
+              "Employee",
+              "Role",
+              "Email",
+              "Phone",
+              "Start Date",
+              "Active",
+              "",
+            ]}
+            tableRows={formattedEmployees}
+          />
+        </div>
       </div>
-    </div>
     </>
   );
 }
